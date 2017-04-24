671b6ab237233ed83d1bfb83b565edd1f4406f9d
Developer-Stanislav <developer.stanislav@gmail.com>
2017-04-24 11:50:56 +0300

    process step 13

--- app/code/local/DS/News/Block/Adminhtml/News/Grid.php
+++ app/code/local/DS/News/Block/Adminhtml/News/Grid.php
@@ -28,12 +28,18 @@ class DS_News_Block_Adminhtml_News_Grid extends Mage_Adminhtml_Block_Widget_Grid
 
         $this->addColumn('status', array(
             'header' => $helper->__('Status'),
-            'index' => 'title',
-            'type' => 'select',
+            'index' => 'status',
+            'type' => 'options',
             'options' => array(
                 0 => $helper->__('No'),
                 1 => $helper->__('Yes')
             )
+
+        /*    'type' => 'options',
+            'options' => array(
+        '1' => Mage::helper('catalog')->__('Yes'),
+        '0' => Mage::helper('catalog')->__('No'),
+    ),*/
         ));
 
         $this->addColumn('created', array(
@@ -44,5 +50,36 @@ class DS_News_Block_Adminhtml_News_Grid extends Mage_Adminhtml_Block_Widget_Grid
 
         return parent::_prepareColumns();
     }
+
+    protected function _prepareMassaction()
+    {
+        $items = $this->getMassactionBlock()->getItems();
+
+
+        $this->setMassactionIdField('news_id');
+        $this->getMassactionBlock()->setFormFieldName('news');
+        $this->getMassactionBlock()
+            ->addItem('enable', array(
+                'label' => $this->__('Enable'),
+                'url' => $this->getUrl('*/*/massEnable'),
+            ))
+            ->addItem('disable', array(
+                'label' => $this->__('Disable'),
+                'url' => $this->getUrl('*/*/massDisable'),
+            ))
+            ->addItem('delete', array(
+                    'label' => $this->__('Delete'),
+                    'url' => $this->getUrl('*/*/massDelete'),
+                )
+            );
+        return $this;
+    }
+    public function getRowUrl($model)
+    {
+        return $this->getUrl('*/*/edit', array(
+            'id' => $model->getId(),
+        ));
+    }
+
 
 }
