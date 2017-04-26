a75eb62f287fdffec02069b7d81b25b2712e9ea6
Developer-Stanislav <developer.stanislav@gmail.com>
2017-04-26 18:18:08 +0300

    Module DS_InvoiceEmail basic code

---
+++ app/code/local/DS/InvoiceEmail/sql/ds_invoice_email_setup/install-0.1.0.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Stanislav Dergunov <developer.stanislav@gmail.com>
+ * @copyright Copyright (c) 2017, Stanislav Dergunov
+ */
+$this->startSetup();
+$this->removeAttribute('customer', 'invoice_email');
+$this->addAttribute('customer', 'invoice_email', array(
+    'label' => 'Switch on invoice mails',
+    'visible' => true,
+    'required' => false,
+    'position' => 511,
+    'type' => 'int',
+    'default' => false,
+    'input' => 'boolean',
+));
+
+Mage::getSingleton('eav/config')
+    ->getAttribute('customer', 'invoice_email')
+    ->setData('used_in_forms', array('adminhtml_customer', 'customer_account_edit'))
+    ->save();
+
+$this->endSetup();
